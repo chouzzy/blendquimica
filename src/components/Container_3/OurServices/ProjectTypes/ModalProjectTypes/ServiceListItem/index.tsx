796c5b0867ed0b60a7c57bacff1b4e60440441f4
chouzzy <mathfernando@hotmail.com>
2025-03-20 20:04:16 -0300

    nova blend - imports resolvidos 7

--- src/components/Container_3/OurServices/ProjectTypes/ModalProjectTypes/ServiceListItem/index.tsx
+++ src/components/Container_3/OurServices/ProjectTypes/ModalProjectTypes/ServiceListItem/index.tsx
@@ -5,10 +5,10 @@ import { AiOutlineCheck } from "react-icons/ai";
 export function ServiceListItem(Props) { 
    return (
       <>
-         <ListItem fontWeight='400' textTransform='uppercase' fontSize='0.85rem'>
+         {/* <ListItem fontWeight='400' textTransform='uppercase' fontSize='0.85rem'>
             <ListIcon as={AiOutlineCheck} fontSize='1.2rem' color='green.400'/>
             {Props.text}
-         </ListItem>
+         </ListItem> */}
       </>
    )
 }
