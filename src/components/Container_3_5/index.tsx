daf4efe5cea6f92df601df2b40b70593e47d547f
chouzzy <mathfernando@hotmail.com>
2025-04-24 12:25:09 -0300

    teste com 2 botões

--- src/components/Container_3_5/index.tsx
+++ src/components/Container_3_5/index.tsx
@@ -32,6 +32,13 @@ export function Container_3_5({ id }: Container_3_5Props) {
                </Button>
             </Link>
          </Flex>
+         <Flex w='100%' p={8} alignItems='center' justifyContent={'center'}>
+            <Link href={whatsappLink(useRouter().pathname)} target='_blank' isExternal>
+               <Button id={'zap-tag'} color='white' bgColor='green.500' _hover={{ bgColor: 'datBlue' }} borderRadius={16} px={8}>
+                  ENTRAR EM CONTATO
+               </Button>
+            </Link>
+         </Flex>
       </Container >
    )
 }
